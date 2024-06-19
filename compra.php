dbb9beef8839f9623df6e69b1168f18472609309
Carleoinserra <inserracarlo@gmail.com>
2024-06-19 10:38:15 +0200

    Add files via upload

--- compra.php
+++ compra.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $db = new SQLite3('MyDb.db');
 
 
@@ -24,6 +26,7 @@ while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
 
 
 }
+
     
 
 
@@ -37,8 +40,23 @@ for ($i = 0; $i < $lunghezza; $i++) {
     echo "Ordine: ". ($i +1) .  " " . $listaModelli[$i] . "<br>";
     echo "In " . $listaPezzi[$i] . " pezzi" . "<br>";
 }
+$_SESSION["listaM"] = $listaModelli;
+$_SESSION["listaP"] = $listaPezzi;
+$_SESSION["somma"] = $somma;
+
 
 echo("<h3> Il totale da pagare è: </h3>");
 echo("<h3>" . $somma . " euro </h3>");
+echo("<br>");
+echo("<br>");
+
+echo("<form action='conferma.php' method = 'post' >");
+echo("<h3> Per confermare il tuo ordine inserisci la tua mail: </h3>");
+
+echo("<input type= 'text' name = 'mail'>");
+echo("<br>");
+echo("<br>");
+echo("<input type = 'submit' value = 'conferma'>");
+echo("</form>");
 
 ?>
